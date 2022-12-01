ecf135dd8b2c3aec54f3a16ecb8ce2a92778a3af
Simon <78769000+simpatbos@users.noreply.github.com>
2022-12-01 14:42:15 +0000

    Permissions

---
+++ main.js
@@ -0,0 +1,11 @@
+onload(e => {
+    chrome.permissions.request({
+        permissions: ["tabs"]
+    }), (granted) => {
+        if (granted) => {
+            alert("permissions granted")
+        } else {
+            alert("not granted")
+        }
+    }
+})
